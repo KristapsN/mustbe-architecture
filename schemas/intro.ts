f52508eba2dd07f5ef28561e1b474aa8ea6e22d9
kuztom <87023457+kuztom@users.noreply.github.com>
2026-01-06 07:38:05 +0200

    Test schema change

--- schemas/intro.ts
+++ schemas/intro.ts
@@ -5,12 +5,22 @@ export default {
   fields: [
     {
       name: 'first',
-      title: 'First description',
+      title: 'First description (lv)',
+      type: 'string',
+    },
+    {
+      name: 'first_en',
+      title: 'First description (en)',
       type: 'string',
     },
     {
       name: 'second',
-      title: 'Second description',
+      title: 'Second description (lv)',
+      type: 'string',
+    },
+    {
+      name: 'second_en',
+      title: 'Second description (en)',
       type: 'string',
     },
   ]
